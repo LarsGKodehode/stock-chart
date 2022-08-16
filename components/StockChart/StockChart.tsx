6395f356feea43503e6d6612febe687821c81b40
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-08-16 09:45:12 +0200

    first steps

---
+++ components/StockChart/StockChart.tsx
@@ -0,0 +1,17 @@
+// Library
+import React from 'react';
+import Highcharts from 'highcharts';
+
+
+// Types
+interface ComponentStockChart {
+  URLEndpoint: string,
+};
+
+function StockChart(props: ComponentStockChart): JSX.Element {
+  return(
+    <div>
+      <h1>Chart here</h1>
+    </div>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
 /**
  * Pure wrapper for component
  */
 
 function App() {
 
+=======
+import './App.css'
+
+function App() {
+>>>>>>> 80cf32c9133fe9446854c93efc3ec9b184ad22d2
   return (
     <div className="App">
     </div>
